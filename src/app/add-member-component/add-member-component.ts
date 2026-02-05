fdc50a6edcff9e858823e5ad0677a39fc921a8b6
vmantai94-alt <v.mantai94@gmail.com>
2026-02-05 12:54:13 +0100

    components added

---
+++ src/app/add-member-component/add-member-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-member-component',
+  imports: [],
+  templateUrl: './add-member-component.html',
+  styleUrl: './add-member-component.css',
+})
+export class AddMemberComponent {
+
+}
